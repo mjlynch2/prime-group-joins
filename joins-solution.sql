4ea60f5f88038b3bcc9dff4da36a0ccd7a84a3c6
mjlynch2 <51831783+mjlynch2@users.noreply.github.com>
2019-10-01 11:53:31 -0500

    STRETCH MODE: Completed questions 9 & 10

--- joins-solution.sql
+++ joins-solution.sql
@@ -15,9 +15,10 @@ JOIN products ON products.id = warehouse_product.product_id
 WHERE products.description = 'cheetos';
 
 --	5.	Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-SELECT * FROM customers
+SELECT customers.first_name, count(*) AS total_orders FROM customers
 JOIN addresses ON customers.id = addresses.customer_id
-JOIN orders on orders.address_id = addresses.id;
+JOIN orders on orders.address_id = addresses.id
+GROUP BY customers.first_name;
 
 --	6.	How many customers do we have?
 SELECT count(*) FROM customers;
@@ -31,3 +32,20 @@ JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
 JOIN products ON products.id = warehouse_product.product_id
 WHERE products.description = 'diet pepsi';
 
+--Stretch
+--	9.	How much was the total cost for each order?
+SELECT (line_items.order_id), sum(products.unit_price * line_items.quantity) AS total_cost FROM products
+JOIN line_items ON line_items.product_id = products.id
+GROUP BY line_items.order_id
+ORDER BY line_items.order_id;
+
+--	10.	How much has each customer spent in total?
+SELECT (customers.first_name), sum(products.unit_price * line_items.quantity) AS total_cost FROM products
+JOIN line_items ON line_items.product_id = products.id
+JOIN orders ON orders.id = line_items.order_id
+JOIN addresses ON addresses.id = orders.address_id
+JOIN customers ON customers.id = addresses.customer_id
+GROUP BY customers.first_name
+ORDER BY total_cost;
+
+--	11.	How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
